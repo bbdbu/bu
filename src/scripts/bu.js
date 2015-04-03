d85782bb13efebe264de596f6d2430d9839b58ed
Minsu Kang <minsu@bbdbu.com>
2015-04-03 14:57:30 +0900

    update

--- src/scripts/bu.js
+++ src/scripts/bu.js
@@ -7,18 +7,5 @@ angular.module('bu', [])
   // window resize event //
   angular.element($window).bind('resize orientationchange', _.throttle(function(e) {
     $e.fire('bu', 'BU:RESIZE');
-    $rootScope.$apply(function() { /* force dirty check */ });
   }, $settings.RESIZE_INTERVAL, { leading: false, trailing: true }));
 }]);
-
-// bu-lang="en"
-// bu-lang="ko"
-// font-loading basic styles .., 400, 700
-
-// h1, h2, h3, h4, h5: sans
-// pre code : fixed
-// p table (header: sans, content: serif)
-// quote (serif)
-// ISO 639 two letter codes for languages
-//
-
